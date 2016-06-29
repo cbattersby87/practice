4e90c9279dc5496d1f59cef90740746af6c84a4b
Charlotte Battersby <cbattersby@cbattersby-mlt.attlocal.net>
2016-06-29 13:38:20 -0700

    adding new express app called library, this shows how to build web applications with node.js and express

--- libraryWithExpress/app.js
+++ libraryWithExpress/app.js
@@ -1,19 +1,21 @@
 var express = require('express');
+
 var app = express();
 
-app.use(express.static('public'));
-app.use(express.static('src/views'));
+var port = process.env.PORT || 5000;
 
-var port = process.env.PORT || 3000;
+app.use(express.static('public'));
+app.set('views', './src/views');
+app.set('view engine', 'jade');
 
 app.get('/', function (req, res) {
-    res.send('Hello World');
+    res.send("Hello World");
 });
 
 app.get('/books', function (req, res) {
-    res.send('Hello Books');
+    res.render('index', {list: ['a','b']});
 });
 
-app.listen(port, function(){
-    console.log('running server on port 3000');
+app.listen(port, function (err) {
+    console.log('running server on port ' + port);
 });
--- libraryWithExpress/gulpfile.js
+++ libraryWithExpress/gulpfile.js
@@ -5,23 +5,22 @@ var nodemon = require('gulp-nodemon');
 
 var jsFiles = ['*.js', 'src/**/*.js'];
 
-gulp.task('style', function () {
+gulp.task('style', function(){
     return gulp.src(jsFiles)
-        .pipe(jshint())
-        .pipe(jshint.reporter('jshint-stylish', {
-            verbose: true
-        }))
-        .pipe(jscs());
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish', {
+        verbose: true
+    }))
+    .pipe(jscs());
 });
 
-gulp.task('inject', function () {
+gulp.task('inject', function(){
     var wiredep = require('wiredep').stream;
     var inject = require('gulp-inject');
 
-    var injectSrc = gulp.src(['./public/css/*.css',
-                              './public/js/*.js'],
-                              {read:false});
-
+    var injectSrc = gulp.src(['./public/css/*.css', 
+                              './public/js/*.js'], {read: false});
+    
     var injectOptions = {
         ignorePath: '/public'
     };
@@ -32,7 +31,7 @@ gulp.task('inject', function () {
         ignorePath: '../../public'
     };
 
-    return gulp.src('./src/views/*.html')
+    return gulp.src('./src/views/*.jade')
         .pipe(wiredep(options))
         .pipe(inject(injectSrc, injectOptions))
         .pipe(gulp.dest('./src/views'));
@@ -41,15 +40,15 @@ gulp.task('inject', function () {
 gulp.task('serve', ['style', 'inject'], function(){
     var options = {
         script: 'app.js',
-        delayTime: 1,
+        delayTime: 1, 
         env: {
-            'PORT' : 3000
+            'PORT': 5000
         },
         watch: jsFiles
-    }
+    };
 
     return nodemon(options)
         .on('restart', function(ev){
-            console.log('Restarting...');
-        })
+            console.log('Restarting... ');
+        });
 });
