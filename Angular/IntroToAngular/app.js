475d7bcb2765eebceb9de9c34a5dbe66e7696dbb
Charlotte Battersby <cbattersby@cbattersby-mlt.attlocal.net>
2016-06-30 10:02:48 -0700

    adding new angular project

---
+++ Angular/IntroToAngular/app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+app.set('views', './src/views');
+app.set('view engine', 'jade');
+
+//routes us to src/views/index.jade
+app.get('/', function(req, res){
+    res.render('index');
+});
+
+//lets open the doors
+app.listen(3000, function (){
+    console.log("App is up and running on port 3000");
+})
